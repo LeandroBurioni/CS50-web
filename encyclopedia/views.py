04eb5c6d589f55fc2dff1de40a6ec25c2ee1ebc2
LeandroBurioni <LeandroBurioni@gmail.com>
2020-08-07 20:53:53 -0300

    Entry Page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from . import util
 
@@ -8,3 +9,32 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def css(request):
+    return render(request, "encyclopedia/css.html", {
+        "entry": util.get_entry("CSS")
+    })
+
+def django(request):
+    return render(request, "encyclopedia/django.html", {
+        "entry": util.get_entry("Django")
+    })
+
+def git(request):
+    return render(request, "encyclopedia/git.html", {
+        "entry": util.get_entry("Git")
+    })
+
+def html(request):
+    return render(request, "encyclopedia/html.html", {
+        "entry": util.get_entry("HTML")
+    })
+
+def python(request):
+    return render(request, "encyclopedia/python.html", {
+        "entry": util.get_entry("Python")
+    })
+
+def noesta(request, noesta):
+    return render(request, "encyclopedia/noesta.html",{
+        "noesta": noesta
+    })
