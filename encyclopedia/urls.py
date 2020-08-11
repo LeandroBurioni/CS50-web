ee2358e11ecf9fd32e6f2be0b9ae7ad56664324a
LeandroBurioni <LeandroBurioni@gmail.com>
2020-08-11 11:19:07 -0300

    repaired entry and index page with DRY

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,12 +5,7 @@ from . import views
 app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/CSS", views.css, name="css"),
-    path("wiki/Django", views.django, name="django"),
-    path("wiki/Git", views.git, name="git"),
-    path("wiki/HTML", views.html, name="html"),
-    path("wiki/Python", views.python, name="python"),
-    path("wiki/<str:search>", views.notfound, name="notfound"),
+    path("wiki/<str:entry>", views.search, name="search"),
     
 
     
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -9,32 +9,16 @@ def index(request):
         "entries": util.list_entries(), 
     })
 
-def css(request):
-    return render(request, "encyclopedia/css.html", {
-        "entry": util.get_entry("css")
-    })
-
-def django(request):
-    return render(request, "encyclopedia/django.html", {
-        "entry": util.get_entry("django")
-    })
-
-def git(request):
-    return render(request, "encyclopedia/git.html", {
-        "entry": util.get_entry("Git")
-    })
-
-def html(request):
-    return render(request, "encyclopedia/html.html", {
-        "entry": util.get_entry("HTML")
-    })
-
-def python(request):
-    return render(request, "encyclopedia/python.html", {
-        "entry": util.get_entry("Python")
-    })
-
-def notfound(request, search):
-    return render(request, "encyclopedia/notfound.html",{
-        "search": search
-    })
+def search(request, entry):
+    if entry == "CSS":
+        return render(request, "encyclopedia/css.html", {"entry": util.get_entry("css")})
+    elif entry == "Django":
+        return render(request, "encyclopedia/django.html", {"entry": util.get_entry("django")})
+    elif entry == "Git":
+        return render(request, "encyclopedia/git.html", {"entry": util.get_entry("Git")})
+    elif entry == "HTML":
+        return render(request, "encyclopedia/html.html", {"entry": util.get_entry("HTML")})
+    elif entry == "Python":
+        return render(request, "encyclopedia/python.html", {"entry": util.get_entry("Python")})
+    else:
+        return render(request, "encyclopedia/notfound.html",{"entry": entry})
