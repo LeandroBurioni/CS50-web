9ed9ba6d246e9ac990852debe647a61055387a09
LeandroBurioni <LeandroBurioni@gmail.com>
2020-08-12 20:03:03 -0300

    entry and index pages

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -9,7 +9,11 @@ def index(request):
     })
 
 def search(request, title):
-    entry_html = markdown2.markdown(util.get_entry(title))
-    return render(request, "encyclopedia/entry.html", {
-        "entry_html":entry_html, "entry_title": title
-    })
+    if util.get_entry(title):
+        entry_html = markdown2.markdown(util.get_entry(title))
+        return render(request, "encyclopedia/entry.html", 
+            {"entry_html":entry_html, "entry_title": title} )
+    else:
+        entry_html = "<h1> Not founded element!</h1><p>May be you can create an entry... Thanks!</p>"
+        return render(request, "encyclopedia/entry.html", 
+            {"entry_html": entry_html, "entry_title": "Not Found!"})
