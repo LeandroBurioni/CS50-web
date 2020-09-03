30ad00f5ba10606524c7767d0906691ed98bcd54
Leandro Burioni <LeandroBurioni@gmail.com>
2020-09-03 15:45:47 -0300

    Working comments

--- auctions/models.py
+++ auctions/models.py
@@ -9,15 +9,21 @@ class User(AbstractUser):
 #I can do Inheritance for DRY with owner and date fields but dont know a way to use related_name with it.
 
 class Listing(models.Model):
-    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True)
     url_image = models.URLField(blank=True)
-    #category
+    
     open = models.BooleanField(default=True) #True if the auction is opened
     date = models.DateTimeField(auto_now=True)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) #Deleting a user, all the auctions will be deleted.
+    CAT = (
+        ('NA', 'Not Category Selected'),
+        ('w', 'Weareable'),
+        ('n', 'new'),
+        ('u', 'used'),
+    )
+    category = models.CharField(max_length=2, choices=CAT, blank=True, default='NA', )
 
     def __str__(self):
         return f"#{self.id}: {self.title} ${self.price}"
@@ -25,14 +31,13 @@ class Listing(models.Model):
 class Comment(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE )
     auction = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments") 
-    text = models.TextField
+    text = models.TextField(help_text="Make a comment here.")
     date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.author} said {self.text}"
 
 class Bid(models.Model):
-    id = models.AutoField(primary_key=True)
     price = models.IntegerField
     date = models.DateTimeField(auto_now=True)
     auction = models.ForeignKey(Listing, on_delete=models.CASCADE, default=Listing) 
