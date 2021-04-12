9448575ac4ae09565fe6071732ac000ab955b716
Leandro Burioni <LeandroBurioni@gmail.com>
2021-04-12 19:20:40 -0300

    TRY: sending, without aesthetic

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -6,12 +6,13 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+  document.querySelector("#compose-form").onsubmit = send_mail;
+
   // By default, load the inbox
   load_mailbox('inbox');
 });
 
 function compose_email() {
-
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
@@ -20,11 +21,10 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
-
-  // To POST an email to the API
-  document.querySelector("#compose-form").onsubmit = function(e) {
-    //console.log(`Submiting?`); to check if is working when you click submit 
-    e.preventDefault;
+}
+  
+// To POST an email to the API
+function send_mail(){
     fetch('/emails', {
       method: 'POST',
       body: JSON.stringify({
@@ -39,9 +39,8 @@ function compose_email() {
         console.log(result);
     });
     
-  }
-
 }
+
 
 function load_mailbox(mailbox) {
   
@@ -54,28 +53,24 @@ function load_mailbox(mailbox) {
   body.innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
   //GET the emails
- 
-
   fetch(`/emails/${mailbox}`)
 .then(response => response.json())
 .then(emails => {
     // Print emails in console
     console.log(emails);
-    /*
-    THIS IS NOT WORKING FOR NOW
-    emails.forEach(element => {
-      const mail = document.createElement('div');
-      mail.setAttribute("class", "div-email");
-      mail.innerHTML(`<a id="sender">${element.sender}</a><a id="subject">${element.subject}</a>
-                      <p id="body">${element.body}</p>
-                      <p id="date">${element.timestamp}</p>`);
-
-    document.querySelector('#emails-view').append(mail);
-    });
-
-      */
-});
+    for (var each in emails){
+      let mail = emails[each];
+      addMail(mail);
+    }
   
-
+});
 }
 
+function addMail(element){
+  const div = document.createElement('div');
+  div.className = 'div-mail';
+  div.innerHTML = '<a id="sender">' + element.sender + ' </a>';
+  div.innerHTML += '<a id="subject">' + element.subject +' </a>';
+  div.innerHTML += '<a id="date">' + element.timestamp +' </a>';
+  document.querySelector('#emails-view').append(div);
+}
