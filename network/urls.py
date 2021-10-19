e0a0e614b9780901b09e2d335e25542384db9dd9
LeandroBurioni <LeandroBurioni@gmail.com>
2021-10-19 19:32:58 -0300

    profile page working

--- network/urls.py
+++ network/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
     path("profile/<str:usernm>", views.profile, name="profile"),
+    path("following", views.following, name="following"),
 
 ]
--- network/views.py
+++ network/views.py
@@ -6,7 +6,7 @@ from django.http.response import JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
 from network import forms
-from .models import Following, User, Post
+from .models import Following, User, Post, Like
 from django.contrib.auth.decorators import login_required
 
 def index(request):
@@ -73,15 +73,28 @@ def register(request):
         return render(request, "network/register.html")
 
 @login_required(login_url='login')
-def profile(request, usernm):
-        user_profile = User.objects.get(username=usernm)
-        if request.user != user_profile:
-            flag = False
-            
-            if Following.objects.check(influencer=user_profile, follower=request.user):
-                flag = True
-            return render(request, "network/profile.html", { "follow_flag":flag, "view_profile": User.objects.get(username=user_profile), 
+def profile(request, usernm): #requested by username
+        user_profile = User.objects.get(username=usernm) #get the User from db 
+        n_following = Following.objects.filter(follower=user_profile).count() 
+        n_follows = Following.objects.filter(influencer=user_profile).count()
+        if request.user != user_profile: 
+            flag = False #By default, the user dont follow anyone.
+            if Following.objects.check(influencer=user_profile, follower=request.user): #If the user follow the loaded profile
+                flag = True     #Flag used to choose what button to show (follow/unfollow)
+            return render(request, "network/profile.html", { 
+                "n_following":n_following, "n_follows":n_follows,
+                "follow_flag":flag, "view_profile": User.objects.get(username=user_profile), 
                 "posts": Post.objects.filter(writed_by=user_profile).order_by('-timestamp')})
-        else:
-            return render(request, "network/profile.html", { "follow_flag":"NO!", "view_profile": User.objects.get(username=user_profile), 
-                "posts": Post.objects.filter(writed_by=user_profile).order_by('-timestamp')})
+        else: # It's themself profile! No button should be showed
+            return render(request, "network/profile.html", { 
+                "n_following":n_following, "n_follows":n_follows,
+                "follow_flag":"self-profile", "view_profile": User.objects.get(username=user_profile), 
+                "posts": Post.objects.filter(writed_by=user_profile).order_by('-timestamp')},
+                )
+
+@login_required(login_url='login')
+def following(request):
+    pass
+#    follow = Following.objects.filter(follower=request.user)
+#    return render(request, "network/index.html", {
+#        "posts": Post.objects.filter( writed_by = follow)})
